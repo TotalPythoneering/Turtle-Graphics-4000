6a643932581ccf3e4b87029cef93084f27a4df40
Mr. Randall Nagy <randall.nagy@gmail.com>
2026-09-23 14:43:58 -0400

    Add files via upload

---
+++ Examples/ex_04_title.py
@@ -0,0 +1,16 @@
+# MISSION: The complete set of examples for ''Python 4000 - Turtle Graphics.''
+# STATUS: Public Release
+# VERSION: 0.0.0
+# NOTES: Code: https://github.com/TotalPythoneering/Turtle-Graphics-4000
+# DATE: 2018-08-09 05:18:34
+# FILE: ex_04_title.py
+# AUTHOR: Randall Nagy
+#
+import turtle
+
+turtle.setup(300, 100)
+turtle.title("My Cool Robot")
+
+turtle.done()
+
+
